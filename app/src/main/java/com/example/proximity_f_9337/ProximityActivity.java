a5e0e1e7a6681c744ea6de7c536fe3d0ee78fbfb
Dio VB <perauterbai@gmail.com>
2020-06-27 23:31:15 +0700

    First Commit

---
+++ app/src/main/java/com/example/proximity_f_9337/ProximityActivity.java
@@ -0,0 +1,49 @@
+package com.example.proximity_f_9337;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class ProximityActivity extends AppCompatActivity implements SensorEventListener {
+    private SensorManager mSensorManager;
+    private Sensor mSensor;
+    TextView tvProx;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mSensorManager=(SensorManager) getSystemService(SENSOR_SERVICE);
+        mSensor=mSensorManager.getDefaultSensor(Sensor.TYPE_PROXIMITY);
+
+        tvProx=(TextView) findViewById(R.id.Proxx);
+    }
+    @Override
+    public void onSensorChanged(SensorEvent sensorEvent){
+        Sensor mySensor = sensorEvent.sensor;
+        if(mySensor.getType()==Sensor.TYPE_PROXIMITY){
+            if(sensorEvent.values[0]==0)
+                tvProx.setText("<<<<NEARR>>>>");
+            else
+                tvProx.setText("<<<<FEARR>>>>");
+        }
+    }
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy){
+
+    }
+    public void onResume(){
+        super.onResume();
+        mSensorManager.registerListener(this, mSensor,SensorManager.SENSOR_DELAY_NORMAL);
+    }
+    protected void onPause(){
+        super.onPause();
+        mSensorManager.unregisterListener(this);
+    }
+
+
+}
